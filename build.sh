ae0bf81ddad357ba190900dad698ea39eeb6ca78
Adrian Gschwend <adrian.gschwend@zazuko.com>
2015-05-04 11:13:50 +0200

    Build script enhancements & SPARQL update, this seems to fix the timeout in Fuseki2

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 java -jar ~/workspace.p3/RMLProcessor/target/RMLMapper-0.1.jar -M eCH0071_150101.ttl -O out/complete.nt
+serdi -b -f -i turtle -o ntriples codes.ttl > out/codes.nt
+cat out/*.nt > histgemeinde2015.nt
