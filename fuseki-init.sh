f4a5f65735bf7fd40f29e474f0fdd978a2bf0f4f
Adrian Gschwend <adrian.gschwend@zazuko.com>
2015-06-26 17:26:20 +0200

    CLEAR everything first

--- fuseki-init.sh
+++ fuseki-init.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:/Users/ktk/Applications/Apache/jena-fuseki-1.1.1
+s-update --service http://localhost:3030/bfs/update 'CLEAR default'
 s-put --verbose http://localhost:3030/bfs/data default histgemeinde2015.nt 
