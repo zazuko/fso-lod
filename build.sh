93a23b13af7474bcc171e2066ed1c946f081e73a
Adrian Gschwend <adrian.gschwend@zazuko.com>
2015-06-30 17:04:18 +0200

    Throw away all data we don't need anymore

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 source init.sh
 source fuseki-init.sh
 source fuseki-construct.sh
-cat out/*.nt > complete-histgemeinde2015.nt
+cat out/*.nt | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' > complete-histgemeinde2015.nt
