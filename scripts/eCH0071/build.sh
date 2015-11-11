720d0f989c95745bbe3e7299fb5ff66deae93dfc
Adrian Gschwend <ktk@netlabs.org>
2015-11-11 14:23:38 +0100

    Adding triplecount

--- scripts/eCH0071/build.sh
+++ scripts/eCH0071/build.sh
@@ -7,3 +7,4 @@ source ./scripts/eCH0071/init.sh
 source ./scripts/eCH0071/fuseki-init.sh
 source ./scripts/eCH0071/fuseki-construct.sh
 cat target/*.nt | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' > target/eCH0071-final.nt
+rapper -i guess -c target/eCH0071-final.nt
