ea2704864d4073625a850ac6122a87d3d16bd352
Adrian Gschwend <ktk@netlabs.org>
2015-11-11 15:03:56 +0100

    No longer necessary

--- scripts/eCH0071/constructs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# TODO I think this can be nuked
-roqet -q construct/eCH0071/map_district2canton.sparql -D complete.nt
-roqet -q construct/eCH0071/map_municipality2canton.sparql -D complete.nt
-roqet -q construct/eCH0071/map_municipality2district.sparql -D complete.nt
