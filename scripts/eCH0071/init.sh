c17648130d23a55367244fdea3cad83b9b46a6e2
Adrian Gschwend <ktk@netlabs.org>
2015-11-11 12:53:00 +0100

    Redirect stderr to null as the logging fills travis

--- scripts/eCH0071/init.sh
+++ scripts/eCH0071/init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir -p target
 rm target/*.nt
-java -jar lib/RMLMapper-0.1.jar -M config/eCH0071.ttl -O target/complete.nt
+# redirect stderr to null as we can't get rid of this logging
+java -jar lib/RMLMapper-0.1.jar -M config/eCH0071.ttl -O target/complete.nt 2> /dev/null
 #perl -pi.back -e 's/(id\/canton\/)([A-Z]{2})/\1\L\2/' target/complete.nt
 serdi -b -f -i turtle -o ntriples attic/codes.ttl >> target/complete.nt
 #cat target/*.nt > histgemeinde2015.nt
