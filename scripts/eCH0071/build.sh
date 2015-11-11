a1fe380869fe254a6a48bf153cd4d30046235338
Adrian Gschwend <ktk@netlabs.org>
2015-11-11 11:55:54 +0100

    Final preparations for Travis

--- scripts/eCH0071/build.sh
+++ scripts/eCH0071/build.sh
@@ -4,7 +4,7 @@ if [ -z $FUSEKI_HOST ]
 then
   export FUSEKI_HOST="localhost"
 fi
-source init.sh
-source fuseki-init.sh
-source fuseki-construct.sh
+source ./scripts/eCH0071/init.sh
+source ./scripts/eCH0071/fuseki-init.sh
+source ./scripts/eCH0071/fuseki-construct.sh
 cat target/*.nt | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' > target/eCH0071-final.nt
--- scripts/eCH0071/init.sh
+++ scripts/eCH0071/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir -p target
 rm target/*.nt
 java -jar lib/RMLMapper-0.1.jar -M config/eCH0071.ttl -O target/complete.nt
 #perl -pi.back -e 's/(id\/canton\/)([A-Z]{2})/\1\L\2/' target/complete.nt
