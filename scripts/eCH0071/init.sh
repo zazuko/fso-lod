f16f5656642968e5e3788b83379a2caf4a7a844d
Adrian Gschwend <ktk@netlabs.org>
2015-11-24 22:48:05 +0100

    New index content

--- scripts/eCH0071/init.sh
+++ scripts/eCH0071/init.sh
@@ -5,4 +5,5 @@ rm target/*.nt
 java -jar lib/RMLMapper-0.1.jar -M config/eCH0071.ttl -O target/complete.nt 2> /dev/null
 #perl -pi.back -e 's/(id\/canton\/)([A-Z]{2})/\1\L\2/' target/complete.nt
 serdi -b -f -i turtle -o ntriples attic/codes.ttl >> target/complete.nt
+serdi -b -f -i turtle -o ntriples input/meta/root.ttl >> target/complete.nt
 #cat target/*.nt > histgemeinde2015.nt
