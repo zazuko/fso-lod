f8dd81c4a167357a12be0b82f181cf35258501df
Adrian Gschwend <ktk@netlabs.org>
2016-05-20 13:16:22 +0200

    Renames & content changes

--- scripts/web/build.sh
+++ scripts/web/build.sh
@@ -3,4 +3,5 @@ serdi -i turtle -o ntriples -p a input/web/about.ttl  > target/website.nt
 serdi -i turtle -o ntriples -p b input/web/apps.ttl >> target/website.nt
 serdi -i turtle -o ntriples -p c input/web/contact.ttl >> target/website.nt
 serdi -i turtle -o ntriples -p d input/web/datasets.ttl >> target/website.nt
-serdi -i turtle -o ntriples -p e input/web/root.ttl >> target/website.nt
+serdi -i turtle -o ntriples -p e input/web/linkeddata.ttl >> target/website.nt
+serdi -i turtle -o ntriples -p e input/web/index.ttl >> target/website.nt
