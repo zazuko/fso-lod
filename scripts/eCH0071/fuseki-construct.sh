20894ec27f80870f9a48ac7a384af1df350936c3
Adrian Gschwend <ktk@netlabs.org>
2017-09-26 17:38:09 +0200

    Refactoring, moving to inserts

--- scripts/eCH0071/fuseki-construct.sh
+++ scripts/eCH0071/fuseki-construct.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_district2canton.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_district2canton.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_district2code.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_district2code.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_district2codeoptional.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_district2codeoptional.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2canton.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2canton.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2code.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2code.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2codeoptional.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2codeoptional.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2dbpedia.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2dbpedia.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2district.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2district.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2mutation.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2mutation.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2status.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2status.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2event.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2event.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_district2event.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_district2event.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2munid.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2munid.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2classes.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2classes.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2classes_hack.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2classes_hack.nt
-curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_district2classes.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_district2classes.nt
-#curl -H "Accept: application/n-triples" --data-urlencode query@construct/eCH0071/map_municipality2eingemeindung.sparql http://$FUSEKI_HOST:3030/bfs/sparql -o target/map_municipality2eingemeindung.nt
+source scripts/env.sh
+
+FUSEKI_UPDATE=http://$FUSEKI_HOST:3030/fso/update
+function sparqlu { curl -H "Accept: text/turtle" --data-urlencode update@$1 $2 ; }
+
+sparqlu construct/map_district2canton.sparql $FUSEKI_UPDATE
+sparqlu construct/map_district2code.sparql $FUSEKI_UPDATE
+sparqlu construct/map_district2codeoptional.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2canton.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2code.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2codeoptional.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2dbpedia.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2district.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2mutation.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2status.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2event.sparql $FUSEKI_UPDATE
+sparqlu construct/map_district2event.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2munid.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2classes.sparql $FUSEKI_UPDATE
+sparqlu construct/map_municipality2classes_hack.sparql $FUSEKI_UPDATE
+sparqlu construct/map_district2classes.sparql $FUSEKI_UPDATE
+#sparqlu construct/map_municipality2eingemeindung.sparql $FUSEKI_UPDATE
--- scripts/eCH0071/fuseki-init.sh
+++ scripts/eCH0071/fuseki-init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#docker run -p 3030:3030 -e ADMIN_PASSWORD=cinderella -d stain/jena-fuseki
-#sleep 20
-curl -u admin:cinderella --data "dbType=tdb&dbName=bfs"  http://$FUSEKI_HOST:3030/$/datasets
-#s-update --service http://localhost:3030/bfs/update 'CLEAR default'
-curl -X PUT  -u admin:cinderella -d @target/complete.nt -H "Content-Type: application/n-triples" http://admin:cinderella@$FUSEKI_HOST:3030/bfs/data
+source scripts/env.sh
+curl -u admin:cinderella --data "dbType=tdb&dbName=fso"  http://$FUSEKI_HOST:3030/$/datasets
+curl -i -X DELETE -u admin:cinderella -G http://$FUSEKI_HOST:3030/fso/data --data-urlencode graph=http://example.org/inference
+curl -X PUT  -u admin:cinderella -d @target/complete.nt -H "Content-Type: application/n-triples" http://admin:cinderella@$FUSEKI_HOST:3030/fso/data --data-urlencode graph=http://example.com/fso
 
