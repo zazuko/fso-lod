25f21f9e5e6038e47caba559595a87d2d32644b5
Adrian Gschwend <ktk@netlabs.org>
2017-09-26 18:24:38 +0200

    Adjusted scripts

--- scripts/eCH0071/fuseki-construct.sh
+++ scripts/eCH0071/fuseki-construct.sh
@@ -4,20 +4,20 @@ source scripts/env.sh
 FUSEKI_UPDATE=http://$FUSEKI_HOST:3030/fso/update
 function sparqlu { curl -H "Accept: text/turtle" --data-urlencode update@$1 $2 ; }
 
-sparqlu construct/map_district2canton.sparql $FUSEKI_UPDATE
-sparqlu construct/map_district2code.sparql $FUSEKI_UPDATE
-sparqlu construct/map_district2codeoptional.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2canton.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2code.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2codeoptional.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2dbpedia.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2district.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2mutation.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2status.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2event.sparql $FUSEKI_UPDATE
-sparqlu construct/map_district2event.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2munid.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2classes.sparql $FUSEKI_UPDATE
-sparqlu construct/map_municipality2classes_hack.sparql $FUSEKI_UPDATE
-sparqlu construct/map_district2classes.sparql $FUSEKI_UPDATE
-#sparqlu construct/map_municipality2eingemeindung.sparql $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_district2canton.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_district2code.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_district2codeoptional.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2canton.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2code.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2codeoptional.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2dbpedia.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2district.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2mutation.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2status.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2event.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_district2event.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2munid.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2classes.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_municipality2classes_hack.rq $FUSEKI_UPDATE
+sparqlu construct/eCH0071/map_district2classes.rq $FUSEKI_UPDATE
+#sparqlu construct/eCH0071/map_municipality2eingemeindung.rq $FUSEKI_UPDATE
---
+++ scripts/env.sh
@@ -0,0 +1,1 @@
+FUSEKI_HOST=localhost
