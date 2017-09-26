7572a3a2c5852140d9351bd15a342293e17ce250
Adrian Gschwend <ktk@netlabs.org>
2017-09-26 22:38:32 +0200

    Adjustments

--- scripts/eCH0071/build.sh
+++ scripts/eCH0071/build.sh
@@ -6,5 +6,5 @@ fi
 source ./scripts/eCH0071/init.sh
 source ./scripts/eCH0071/fuseki-init.sh
 source ./scripts/eCH0071/fuseki-construct.sh
-cat target/*.nt | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' > target/eCH0071-final.nt
+cat target/fso-clean-*.nt | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' > target/eCH0071-final.nt
 rapper -i guess -c target/eCH0071-final.nt
