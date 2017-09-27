849224fca374f84646589e08469c09f805b37060
Adrian Gschwend <ktk@netlabs.org>
2017-09-27 19:06:55 +0200

    Adjusted LINDAS endpoint

--- scripts/lindas-put.sh
+++ scripts/lindas-put.sh
@@ -3,5 +3,5 @@ curl -n \
      -X PUT \
      -H Content-Type:application/n-triples \
      -T target/eCH0071-final.nt \
-     -G https://lindasprd.netrics.ch:8443/lindas \
+     -G https://lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/eCH-0071
