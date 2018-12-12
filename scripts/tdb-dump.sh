6727f7ed1a52c1ae077620c47438325ce89524d2
Adrian Gschwend <ktk@netlabs.org>
2018-12-12 23:46:59 +0100

    Kick out temporary triples

--- scripts/tdb-dump.sh
+++ scripts/tdb-dump.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tdbdump --loc target/tdb | serdi -i nquads -o ntriples - | sed '\#example.org#d' | gzip --stdout > target/everything.nt.gz
+tdbdump --loc target/tdb | serdi -i nquads -o ntriples - | sed '\#example.org#d' | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' | gzip --stdout > target/everything.nt.gz
