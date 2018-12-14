77c80fb814e057f75ae8a03f2a16c51df6fe84ce
Adrian Gschwend <ktk@netlabs.org>
2018-12-14 17:17:24 +0100

    Add publication date of XML itself (validFrom)

--- scripts/void-timestamp.sh
+++ scripts/void-timestamp.sh
@@ -1,3 +1,4 @@
 TODAY="$(date "+%Y-%m-%d")"
 NOW="$(date -u "+%Y-%m-%dT%H:%M:%SZ")"
-cat input/void.ttl| sed "s/%%DATEISSUED%%/${TODAY}/g" | sed "s/%%DATECREATED%%/${NOW}/g" > target/void.ttl
+VALIDFROM="$(grep validFrom tmp/eCH0071_180401.xml | cut -f2 -d">" | cut -f1 -d"<")"
+cat input/void.ttl| sed "s/%%DATEISSUED%%/${TODAY}/g" | sed "s/%%DATECREATED%%/${NOW}/g" | sed "s/%%VALIDFROM%%/${VALIDFROM}/g" > target/void.ttl
