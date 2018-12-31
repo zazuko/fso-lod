b193d845fdecbb598a633f4272ee7a5251600fa0
Adrian Gschwend <ktk@netlabs.org>
2018-12-31 01:06:00 +0100

    Remove hardcoded version for metadata

--- scripts/void-timestamp.sh
+++ scripts/void-timestamp.sh
@@ -1,4 +1,4 @@
 TODAY="$(date "+%Y-%m-%d")"
 NOW="$(date -u "+%Y-%m-%dT%H:%M:%SZ")"
-VALIDFROM="$(grep validFrom tmp/eCH0071_180401.xml | cut -f2 -d">" | cut -f1 -d"<")"
+VALIDFROM="$(grep validFrom tmp/eCH0071_*.xml | cut -f2 -d">" | cut -f1 -d"<")"
 cat input/void.ttl| sed "s/%%DATEISSUED%%/${TODAY}/g" | sed "s/%%DATECREATED%%/${NOW}/g" | sed "s/%%VALIDFROM%%/${VALIDFROM}/g" > target/void.ttl
